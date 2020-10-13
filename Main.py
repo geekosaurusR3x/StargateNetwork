f7393303598dc8e0731f7e752958ab80cb7e4790
GeekosaurusR3x <skad@protonmail.com>
2020-10-13 15:10:13 +0200

    Renaming file

--- Main.py
+++ Main.py
@@ -1,8 +1,10 @@
 from StargateNetwork.Stargate import Stargate
 import time
+import sys
 
 if __name__ == "__main__":
     sg = Stargate('', 6842)
     sg.waitForConnection()
     time.sleep(5)
     sg.stop()
+    sys.exit(0)
