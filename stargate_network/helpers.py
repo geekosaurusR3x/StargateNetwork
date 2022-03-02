bcab8cc0717e8d6bc7339420dab6519cc99784f0
GeekosaurusR3x <>
2022-03-02 20:38:45 +0000

    Update helper for having lan adress

--- stargate_network/helpers.py
+++ stargate_network/helpers.py
@@ -182,13 +182,24 @@ class StargateSocket(ABC):
         self.host = None
         self.port = None
 
+
     def getAddress(self) -> str:
         """Retrun the public ip address
 
         Returns:
             str: Ip address
         """
-        return socket.gethostbyname(socket.gethostname())
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.settimeout(0)
+        try:
+            # doesn't even have to be reachable
+            s.connect(('10.255.255.255', 1))
+            IP = s.getsockname()[0]
+        except Exception:
+            IP = '127.0.0.1'
+        finally:
+            s.close()
+        return IP
 
     def loopReceve(self) -> NoReturn:
         """Loop for receiving socket data without blocking
